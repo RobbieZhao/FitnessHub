a0da3b99ad1379789af7eb0347abce2db4c27ddd
Zixuan Zhao <robbiehk131@gmail.com>
2020-09-09 22:52:22 -0600

    Added a intent from main activity to user activity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,18 +1,35 @@
 package com.example.myapplication;
 
-        import androidx.appcompat.app.AppCompatActivity;
-        import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
         import android.util.Log;
         import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-    public void clickFunction(View view){
-        Log.i("Info","Button pressed");
-    }
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button mButtonSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButtonSubmit = findViewById(R.id.button);
+
+        mButtonSubmit.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+
+            case R.id.button: {
+                Intent myIntent = new Intent(MainActivity.this, UsersActivity.class);
+                MainActivity.this.startActivity(myIntent);
+            }
+        }
     }
 }
