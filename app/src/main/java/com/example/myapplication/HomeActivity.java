04a46d9e0c9cdb46ad000c052b93e67d0cdd8322
Zixuan Zhao <robbiehk131@gmail.com>
2020-09-20 14:35:20 -0600

    add gps tracker to home activity

--- app/src/main/java/com/example/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -9,22 +9,33 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.util.List;
+import java.util.Locale;
 
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button mButtonCalculator, mButtonWeather, mButtonMap, mButtonProfile;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private double latitude, longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +51,37 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         mButtonWeather.setOnClickListener(this);
         mButtonMap.setOnClickListener(this);
         mButtonProfile.setOnClickListener(this);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        updateLocation();
+    }
+
+    private void updateLocation() {
+        if (ActivityCompat.checkSelfPermission(HomeActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                @Override
+                public void onComplete(@NonNull Task<Location> task) {
+                    Location location = task.getResult();
+                    if (location != null) {
+                        try {
+                            Geocoder geocoder = new Geocoder(HomeActivity.this,
+                                    Locale.getDefault());
+                            List<Address> addresses = geocoder.getFromLocation(
+                                    location.getLatitude(), location.getLongitude(), 1
+                            );
+
+                            latitude = addresses.get(0).getLatitude();
+                            longitude = addresses.get(0).getLongitude();
+                        } catch (Exception e) {
+
+                        }
+                    }
+                }
+            });
+        } else {
+            ActivityCompat.requestPermissions(HomeActivity.this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+        }
     }
 
     @Override
@@ -51,11 +93,13 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
             }
 
             case R.id.WeatherButton: {
+                updateLocation();
                 openWeather();
                 break;
             }
 
             case R.id.MapButton: {
+                updateLocation();
                 openMap();
                 break;
             }
@@ -77,10 +121,9 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void openMap() {
-        Uri searchUri = Uri.parse("geo:40.767778,-111.845205?q=hike");
-
+        Uri searchUri = Uri.parse("geo:" + latitude + "," + longitude + "?q=hike");
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, searchUri);
-        if(mapIntent.resolveActivity(getPackageManager())!=null){
+        if(mapIntent.resolveActivity(getPackageManager()) != null){
             startActivity(mapIntent);
         }
     }
